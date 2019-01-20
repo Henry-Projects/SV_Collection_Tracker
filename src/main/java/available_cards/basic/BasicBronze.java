1e11ffba9d2b8211dd2fd888deb9147025a5301e
Henry-Projects <henryvu.contact@gmail.com>
2019-01-19 16:28:22 -0800

    testing more enums

--- src/main/java/available_cards/basic/BasicBronze.java
+++ src/main/java/available_cards/basic/BasicBronze.java
@@ -1,15 +1,32 @@
 package available_cards.basic;
-import cardtype.Rarity;
-import java.util.*;
 
-public class BasicBronze {
+import card_types.Rarity;
 
-    static public Rarity
-            bronze1,
-            bronze2,
-            bronze3
-            = Rarity.NORMAL_BRONZE;
+public enum BasicBronze {
 
+    b1("Bronze1"),
+    b2("Bronze2"),
+    b3("Bronze3");
+
+    private String name;
+
+    private BasicBronze(String s){
+        name = s;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public String getRarity(){
+        return String.valueOf(Rarity.NORMAL_BRONZE);
+    }
+
+    public int getLiquefy_values(){
+        return Rarity.NORMAL_BRONZE.getLiquefy_value();
+    }
+
+    public int getCreate_values(){
+        return Rarity.NORMAL_BRONZE.getCreate_value();
+    }
 }
-
-
--- src/main/java/available_cards/basic/BasicSilver.java
+++ src/main/java/available_cards/basic/BasicSilver.java
@@ -1,4 +1,24 @@
 package available_cards.basic;
 
-public class BasicSilver {
+import card_types.Rarity;
+
+public enum BasicSilver {
+
+    silver1,
+    silver2,
+    silver3,
+    silver4;
+
+    public String getRarity(){
+        return String.valueOf(Rarity.NORMAL_SILVER);
+    }
+
+    public int getLiquefy_values(){
+        return Rarity.NORMAL_SILVER.getLiquefy_value();
+    }
+
+    public int getCreate_values(){
+        return Rarity.NORMAL_SILVER.getCreate_value();
+    }
+
 }
